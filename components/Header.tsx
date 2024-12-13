50463343311ea5b7b958d7f1f7378dde8c8da5de
quanghien <nquanghien97@gmail.com>
2024-12-13 17:28:09 +0700

    init commit

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <div>
+          <Image src="/logo-be-cao-khoe.png" alt="logo" width={60} height={60} />
+        </div> 
+      </div>
+      <div>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
